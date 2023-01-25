d3584facc86f53d0d7aedff56387819289719990
CodingWithLaura <laura.ganss@web.de>
2023-01-25 20:26:09 +0100

    Add function in .js file to call flask endpoint

--- html_frontend/index.js
+++ html_frontend/index.js
@@ -1,3 +1,15 @@
-bfunction ShowANumber() {
+function showANumber() {
     document.getElementById("inputfield").value = Math.floor(Math.random() * 10);
+}
+
+function httpGet() {
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+						alert("Huhu");
+				}
+		};
+		var endpoint = "http://127.0.0.1:5000/";
+		xmlHttp.open("GET", endpoint, true);
+		xmlHttp.send(null);
 }
