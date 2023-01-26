ffce8fa4bb1a9350c7d89fbdfb858e84a1d87971
CodingWithLaura <laura.ganss@web.de>
2023-01-26 11:20:04 +0100

    Display random number in html inputfield generated in backend script

--- html_frontend/index.js
+++ html_frontend/index.js
@@ -6,7 +6,7 @@ function httpGet() {
 		var xmlHttp = new XMLHttpRequest();
 		xmlHttp.onreadystatechange = function() {
 				if (this.readyState == 4 && this.status == 200) {
-						alert("Huhu");
+						document.getElementById("inputfield").value = this.responseText;
 				}
 		};
 		var endpoint = "http://127.0.0.1:5000/";
