1d257156320fe17125a8b9c5cfb1d0b3e4542420
CodingWithLaura <laura.ganss@web.de>
2023-01-26 11:20:04 +0100

    Display random number in html inputfield generated in backend script

--- flask_backend/warteapparat_backend.py
+++ flask_backend/warteapparat_backend.py
@@ -2,10 +2,12 @@
 
 from flask import Flask
 from flask_cors import CORS
+import random
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def get_a_number():
-    return "10"
+    random_number = random.randint(0,9)
+    return "{}".format(random_number)
--- html_frontend/index.js
+++ html_frontend/index.js
@@ -3,13 +3,13 @@ function showANumber() {
 }
 
 function httpGet() {
-		var xmlHttp = new XMLHttpRequest();
-		xmlHttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
-						alert("Huhu");
-				}
-		};
-		var endpoint = "http://127.0.0.1:5000/";
-		xmlHttp.open("GET", endpoint, true);
-		xmlHttp.send(null);
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("inputfield").value = this.responseText;
+        }
+    };
+    var endpoint = "http://127.0.0.1:5000/";
+    xmlHttp.open("GET", endpoint, true);
+    xmlHttp.send(null);
 }
