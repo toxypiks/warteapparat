4e032417fc37ce27a5a81e74bad7b3786ef97e88
CodingWithLaura <laura.ganss@web.de>
2023-01-27 21:09:19 +0100

    Remove index.js

--- flask_backend/warteapparat_backend.py
+++ flask_backend/warteapparat_backend.py
@@ -2,12 +2,21 @@
 
 from flask import Flask
 from flask_cors import CORS
+from multiprocessing import Value
 import random
 
 app = Flask(__name__)
 CORS(app)
+counter = Value('i',0)
 
 @app.route("/")
 def get_a_number():
+    with counter.get_lock():
+        counter.value += 1
     random_number = random.randint(0,9)
     return "{}".format(random_number)
+
+@app.route("/state")
+def get_state():
+    state_count = counter.value
+    return "{}".format(state_count)
