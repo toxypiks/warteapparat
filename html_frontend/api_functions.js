a77f18f7ce8385509178692d1142712d33d1323c
CodingWithLaura <laura.ganss@web.de>
2023-01-27 21:10:28 +0100

    Add api function

--- html_frontend/api_functions.js
+++ html_frontend/api_functions.js
@@ -3,6 +3,7 @@ function showANumber() {
 }
 
 function httpGet() {
+<<<<<<< HEAD
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -12,4 +13,27 @@ function httpGet() {
     var endpoint = "http://127.0.0.1:5000/";
     xmlHttp.open("GET", endpoint, true);
     xmlHttp.send(null);
+=======
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+						document.getElementById("inputfield").value = this.responseText;
+				}
+		};
+		var endpoint = "http://127.0.0.1:5000/";
+		xmlHttp.open("GET", endpoint, true);
+		xmlHttp.send(null);
+}
+
+function httpStateGet() {
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+						document.getElementById("display_state").value = this.responseText;
+				}
+		};
+		var endpoint = "http://127.0.0.1:5000/state";
+		xmlHttp.open("GET", endpoint, true);
+		xmlHttp.send(null);
+>>>>>>> 65a599f (Rename html file)
 }
