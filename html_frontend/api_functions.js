59b1919e2d9a94ec5c710c1df9744206f0e3a115
CodingWithLaura <laura.ganss@web.de>
2023-01-27 22:03:23 +0100

    Refactor xhttp request function

--- html_frontend/api_functions.js
+++ html_frontend/api_functions.js
@@ -1,39 +1,19 @@
-function showANumber() {
-    document.getElementById("inputfield").value = Math.floor(Math.random() * 10);
-}
-
-function httpGet() {
-<<<<<<< HEAD
+function httpGet(endpoint_name, display_element_name) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("inputfield").value = this.responseText;
+            document.getElementById(display_element_name).value = this.responseText;
         }
     };
-    var endpoint = "http://127.0.0.1:5000/";
+    var endpoint = "http://127.0.0.1:5000/" + endpoint_name;
     xmlHttp.open("GET", endpoint, true);
     xmlHttp.send(null);
-=======
-		var xmlHttp = new XMLHttpRequest();
-		xmlHttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
-						document.getElementById("inputfield").value = this.responseText;
-				}
-		};
-		var endpoint = "http://127.0.0.1:5000/";
-		xmlHttp.open("GET", endpoint, true);
-		xmlHttp.send(null);
 }
 
-function httpStateGet() {
-		var xmlHttp = new XMLHttpRequest();
-		xmlHttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
-						document.getElementById("display_state").value = this.responseText;
-				}
-		};
-		var endpoint = "http://127.0.0.1:5000/state";
-		xmlHttp.open("GET", endpoint, true);
-		xmlHttp.send(null);
->>>>>>> 65a599f (Rename html file)
+function httpGetRandomNumber() {
+    httpGet("","inputfield");  
+}
+
+function httpGetState() {
+    httpGet("state", "display_state");
 }
