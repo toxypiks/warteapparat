3f8eb34f97f40b2f3c2995f97797b16cfffa7e78
CodingWithLaura <laura.ganss@web.de>
2023-02-24 21:42:33 +0100

    Add function to output all orders as table

--- html_frontend/api_functions.js
+++ html_frontend/api_functions.js
@@ -6,24 +6,46 @@ function outputToConsole(text) {
     para.scrollIntoView();
 }
 
-function tableOutputToConsole(text) {
-    var tbl = document.createElement("table");
-    var tblBody = document.createElement("tbody");
 
-    for(let i = 0; i < 1; i++) {
+function tableOutputToConsole(json_data) {
+    var datensaetze = json_data;
+    var tbl = document.createElement("table");
+    var tblh_tr = document.createElement("tr");
+    var tblBody = document.createElement("tbody");
+    var tblh_order_id = document.createElement("th");
+    var tblh_timestamp = document.createElement("th");
+    var tblh_order_state = document.createElement("th");
+    var tblheader = document.createElement("thead");
+
+    const order_id_text = document.createTextNode("Order ID");
+    const order_timestamp_text = document.createTextNode("Order Time");
+    const order_state_text = document.createTextNode("Order State");
+
+    tblh_order_id.appendChild(order_id_text);
+    tblh_timestamp.appendChild(order_timestamp_text);
+    tblh_order_state.appendChild(order_state_text);
+
+    tblh_tr.appendChild(tblh_order_id);
+    tblh_tr.appendChild(tblh_timestamp);
+    tblh_tr.appendChild(tblh_order_state);
+
+    tblheader.appendChild(tblh_tr);
+    tbl.appendChild(tblheader);
+    
+    for(let i = 0; i < datensaetze.length; i++) {
         const row = document.createElement("tr");
 
-        for (let j = 0; j < text.length; j++) {
+        for (let j = 0; j < datensaetze[i].length; j++) {
             const cell = document.createElement("td");
-            const cellText = document.createTextNode(text[j]);
+            const cellText = document.createTextNode(datensaetze[i][j] );
             cell.appendChild(cellText);                       
             row.appendChild(cell);
         }
         tblBody.appendChild(row);
     }
     tbl.appendChild(tblBody);
-    document.body.appendChild(tbl);
     tbl.setAttribute("border", "2");
+    document.getElementById("console").appendChild(tbl);
 }
     
 
@@ -35,7 +57,14 @@ function httpGet(endpoint_name, variable_context) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            tableOutputToConsole(this.responseText);
+            // ToDo: für verschiede anfragen verschiedenes Handling
+            // register handler
+            if(variable_context == "all orders: ") {
+                var json_data = JSON.parse(this.responseText);
+                tableOutputToConsole(json_data);
+            } else {
+                outputToConsole(variable_context + this.responseText);
+            }          
         }
     };
     var endpoint = "http://127.0.0.1:5000/" + endpoint_name;
@@ -47,6 +76,8 @@ function httpPost(endpoint_name, param_name, param_value, variable_context) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+            var jsonResponse = JSON.parse(this.responseText);
+            // ToDo
             outputToConsole(variable_context + this.responseText);
         }
     };
