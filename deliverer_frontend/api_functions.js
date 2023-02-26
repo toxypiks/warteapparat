2bfb8dda4b2cfb4f2a6a31de2ab3d8f97a498d49
CodingWithLaura <laura.ganss@web.de>
2023-02-26 15:51:28 +0100

    Add tableOutputToConsole function

--- deliverer_frontend/api_functions.js
+++ deliverer_frontend/api_functions.js
@@ -6,15 +6,52 @@ function outputToConsole(text) {
     para.scrollIntoView();
 }
 
+function tableOutputToConsole(json_data, row_header) {
+    var datensaetze = json_data;
+    var tbl = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+    var tblheader = document.createElement("thead");
+    var tblh_tr = document.createElement("tr");
+
+    for (let x = 0; x < row_header.length; x++) {
+        const row_header_text = document.createTextNode(row_header[x]);
+        var tblh_element = document.createElement("th");
+        tblh_element.appendChild(row_header_text);
+        tblh_tr.appendChild(tblh_element);
+    }
+
+    tblheader.appendChild(tblh_tr);
+    tbl.appendChild(tblheader);
+    
+    for(let i = 0; i < datensaetze.length; i++) {
+        const row = document.createElement("tr");
+
+        for (let j = 0; j < datensaetze[i].length; j++) {
+            const cell = document.createElement("td");
+            const cellText = document.createTextNode(datensaetze[i][j] );
+            cell.appendChild(cellText);                       
+            row.appendChild(cell);
+        }
+        tblBody.appendChild(row);
+    }
+    tbl.appendChild(tblBody);
+    tbl.setAttribute("border", "2");
+    document.getElementById("console").appendChild(tbl);
+}
+
 function clearConsole() {
     document.getElementById("console").innerHTML = "";
 }
 
-function httpGet(endpoint_name, variable_context) {
+function clearConsole() {
+    document.getElementById("console").innerHTML = "";
+}
+
+function httpGet(endpoint_name, variable_context, response_handler) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            outputToConsole(variable_context + this.responseText);
+            response_handler(this.responseText);         
         }
     };
     var endpoint = "http://127.0.0.1:5000/" + endpoint_name;
@@ -22,17 +59,17 @@ function httpGet(endpoint_name, variable_context) {
     xmlHttp.send(null);
 }
 
-function httpPost(endpoint_name, param_name, param_value, variable_context) {
+function httpPost(endpoint_name, param_name, param_value, variable_context, response_handler) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            outputToConsole(variable_context + this.responseText);
+            response_handler(this.responseText);
         }
     };
     var endpoint = "http://127.0.0.1:5000/" + endpoint_name;
     xmlHttp.open("POST", endpoint, true);
     xmlHttp.setRequestHeader('Content-type', 'application/json');
-    var param_json = "{ \"" + param_name + "\" : \"" + param_value + "\" }"
+    var param_json = "{ \"" + param_name + "\" : \"" + param_value + "\" }";
     xmlHttp.send(param_json);
 }
 
@@ -43,7 +80,12 @@ function httpPostNumberOfReadyPizzas() {
 }
 
 function httpGetAllOrderedOrdersSortedByOrderTime() {
-    httpGet("get_ordered_orders_sorted_by_order_time", "all orders sorted by order time: ");
+    var response_handler = (response_text) => {
+        var json_data = JSON.parse(response_text);
+        var table_header = ["Order ID", "Order Time", "Order State"];
+        tableOutputToConsole(json_data, table_header);
+    };
+    httpGet("get_ordered_orders_sorted_by_order_time", "all orders sorted by order time: ", response_handler);
 }
 
 outputToConsole("init");
